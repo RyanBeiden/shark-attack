91e17c03736fe0a31ea7973641daa2c598f85713
Ryan Beiden <rdbeiden@gmail.com>
2020-08-13 19:15:41 -0500

    add functionality to print students with `isDead` set to `true`

--- src/components/GraveStone/GraveStone.js
+++ src/components/GraveStone/GraveStone.js
@@ -1,1 +1,20 @@
+import React from 'react';
+
+import studentShape from '../../helpers/props/studentShape';
 import './GraveStone.scss';
+
+class GraveStone extends React.Component {
+  static propType = {
+    students: studentShape.studentShape,
+  };
+
+  render() {
+    const { student } = this.props;
+
+    return (
+      <p>{student.firstName} {student.lastName}</p>
+    );
+  }
+}
+
+export default GraveStone;
--- src/components/Graveyard/Graveyard.js
+++ src/components/Graveyard/Graveyard.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import studentShape from '../../helpers/props/studentShape';
+import GraveStone from '../GraveStone/GraveStone';
+
 import './Graveyard.scss';
+
+class Graveyard extends React.Component {
+  static propTypes = {
+    students: PropTypes.arrayOf(studentShape.studentShape),
+  }
+
+  render() {
+    const { dearlyBeloved } = this.props;
+    const studentCards = dearlyBeloved.map((student) => (
+      <GraveStone key={student.id} student={student} />
+    ));
+
+    return (
+      <div>
+        { studentCards }
+      </div>
+    );
+  }
+}
+
+export default Graveyard;
